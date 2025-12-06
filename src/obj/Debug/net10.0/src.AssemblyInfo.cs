6274ea2462e9851a326d9052e8c2697b47460739
Jakub Hoepfner <98704474+Kabooshki@users.noreply.github.com>
2025-12-06 20:23:06 +0000

    Implemented Basic Customisation Menu
    Customisation menu taking any string inputs as clothing. Item Class was also implemented to make a start on how items should be stored and used.

--- src/obj/Debug/net10.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5be8757f15d6832ed7ca92dc7a3053b475a069e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f34903bba16ac2e51cfcfd89b202b59dcd11c186")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
